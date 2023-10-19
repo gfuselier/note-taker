c3ae311c55f9310115de39fe04c454a727a011fa
gfuselier <gisellefuselier@gmail.com>
2023-10-19 09:04:28 -0500

    added get notes route

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //get /notes should return notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
 // get /api/notes should read db.json and return all saved notes as json
 // post /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved. Look for npm packages that do this
 //get * should return index.html
