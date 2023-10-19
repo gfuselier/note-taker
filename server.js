b45fd9fd52ed060d57ca0cbdac72edeb90d9fd32
gfuselier <gisellefuselier@gmail.com>
2023-10-19 13:37:10 -0500

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
